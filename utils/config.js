a5b3b47b9bf6a2f2ba3ff0ff9320622d31267215
Gulnara Fedorova <gul-khurmatova@yandex.ru>
2023-10-26 21:40:43 +0200

    дипломный проект, создание проекта

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const {
-  DB_URL = 'mongodb://localhost:27017/bitfilmsdb',
+  DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
   PORT = 3000,
   NODE_ENV,
   JWT_SECRET,
